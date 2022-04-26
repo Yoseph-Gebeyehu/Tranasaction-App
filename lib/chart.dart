872e2e4073c4d00088be3b78bd80b996aef5b0c2
Yoseph Gebeyehu <yosephgebeyehu73@gmail.com>
2022-04-26 17:29:52 +0300

    The lists are made to be generated authomaticaly

--- lib/chart.dart
+++ lib/chart.dart
@@ -4,32 +4,43 @@ import '/transaction.dart';
 
 class Chart extends StatelessWidget {
   // const Chart({ Key? key }) : super(key: key);
-  final List<Transaction> _trnsaction;
-  Chart(this._trnsaction);
+  final List<Transaction> _recentTransaction;
+  Chart(this._recentTransaction);
+  List<Map<String, Object>> get getList {
+    return List.generate(7, (index) {
+      DateTime dayOfWeek = DateTime.now().subtract(Duration(days: index));
+      double amount;
+      for (int i = 0; i < _recentTransaction.length; i++) {
+        if (_recentTransaction[i].date.day == dayOfWeek.day &&
+            _recentTransaction[i].date.month == dayOfWeek.month &&
+            _recentTransaction[i].date.year == dayOfWeek.year) {
+          amount = _recentTransaction[i].amount;
+        }
+      }
+      ;
+      return {'day': DateFormat.E().format(dayOfWeek), 'amount': amount};
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: _trnsaction.map((tx) {
-        return Column(
-          children: [
-            Text(
-              tx.amount.toString(),
-            ),
-            SizedBox(
-              height: 4,
-            ),
-            Container(
-              height: 10,
-              color: Colors.amber,
-            ),
-            SizedBox(height: 4),
-            Text(
-              DateFormat().add_E().format(tx.date),
-            )
-          ],
-        );
-      }).toList(),
+    return Card(
+      elevation: 8,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: getList.map((tx) {
+          return Column(
+            children: [
+              Text(tx['amount'].toString()),
+              SizedBox(
+                height: 50,
+              ),
+              Text(tx['day'].toString()),
+            ],
+          );
+        }).toList(),
+      ),
     );
+    
   }
 }
